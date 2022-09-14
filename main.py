093a19f33a78c5ac49ee58848a1a935a4a8f2adb
cassoudebatarcilae <fs>
2022-09-14 08:52:39 -0400

    Ajouter fonction count_word.

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = input("Qu'elle est ta phrase")
-print(f'La phrase a {text.count(" ") + 1 + text.count("-")} mots')
+def count_word(phrase: str) -> int:
+    return phrase.count(" ") + 1 + phrase.count("-")
 
 
+sentence = input("Qu'elle est ta phrase ?\nRep: ")
+print(f'La phrase a {count_word(sentence)} mots.')
